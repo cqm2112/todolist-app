fa725b9e7d3b4d657f35a7b9c1becaec4da0077e
Cristopher Quiroz Minier <Cristopher.Quiroz@farmaciacarol.com>
2023-11-13 00:06:23 -0400

    update links and certificates

--- global.js
+++ global.js
@@ -1,3 +1,7 @@
-const ApiUrl = "http://cqm2121-001-site1.htempurl.com/api/Todo";
+const ApiUrl = "https://cqm2121-001-site1.htempurl.com/api/Todo";
+const UrlEncuesta = "https://forms.gle/fc8mbaYhwr7kAZQz9";
+const ApiGitHub = "https://github.com/cqm2112/TodoApi"
+const FrontGitHub = "https://github.com/cqm2112/todolist-app"
+const TelemedikLink = "https://www.telemedik.com/"
 
-export { ApiUrl };
+export { ApiUrl, UrlEncuesta, TelemedikLink, ApiGitHub, FrontGitHub };
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,6 +2,7 @@ import Modal from 'react-bootstrap/Modal';
 import { Button, ListGroup } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useAuth } from "../authContext";
+import { ApiGitHub, FrontGitHub, UrlEncuesta, } from '../../global';
 
 
 function Menu(props) {
@@ -67,12 +68,9 @@ function Menu(props) {
                     <ListGroup>
 
                         <ListGroup.Item className='text-center font-bold'>Inicia sesion</ListGroup.Item>
-                        <ListGroup.Item className='font-bold'> <label>Codigo Fuente Front: <a href='https://github.com/' target='_Blank'> Aqui</a> </label></ListGroup.Item>
-                        <ListGroup.Item className='font-bold'> <label>Codigo Fuente Api: <a href='https://github.com/' target='_Blank'> Aqui</a> </label></ListGroup.Item>
-
-                        <ListGroup.Item className='font-bold'> <label>Api: <a href='https://github.com/' target='_Blank'> Aqui</a> </label></ListGroup.Item>
-
-                        <ListGroup.Item className='font-bold'> <label>Valorar prueba tecnica <a href='https://github.com/' target='_Blank'> Aqui</a> </label></ListGroup.Item>
+                        <ListGroup.Item className='font-bold'> <label>Codigo Fuente Front: <a href={FrontGitHub} target='_Blank'> Aqui</a> </label></ListGroup.Item>
+                        <ListGroup.Item className='font-bold'> <label>Codigo Fuente Api: <a href={ApiGitHub} target='_Blank'> Aqui</a> </label></ListGroup.Item>
+                        <ListGroup.Item className='font-bold'> <label>Valorar prueba tecnica <a href={UrlEncuesta}target='_Blank'> Aqui</a> </label></ListGroup.Item>
 
                         <ListGroup.Item className='font-bold'> <label>Telemedik: <a href='https://github.com/' target='_Blank'> Aqui</a> </label></ListGroup.Item>
                     </ListGroup>
