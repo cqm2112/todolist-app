52901992865ab98a8e898b0135551b1453349bb6
Cristopher Quiroz Minier <Cristopher.Quiroz@farmaciacarol.com>
2023-11-13 00:20:02 -0400

    update missing links

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,7 +2,7 @@ import Modal from 'react-bootstrap/Modal';
 import { Button, ListGroup } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useAuth } from "../authContext";
-import { ApiGitHub, FrontGitHub, UrlEncuesta, } from '../../global';
+import { ApiGitHub, FrontGitHub, TelemedikLink, UrlEncuesta, } from '../../global';
 
 
 function Menu(props) {
@@ -32,14 +32,12 @@ function Menu(props) {
                     <ListGroup>
 
                         <ListGroup.Item className='text-center font-bold'> <label>Codigo secreto: </label> {token}</ListGroup.Item>
-                        <ListGroup.Item className='font-bold'> <label>Codigo Fuente Front: <a href='https://github.com/' target='_Blank'> Aqui</a> </label></ListGroup.Item>
-                        <ListGroup.Item className='font-bold'> <label>Codigo Fuente Api: <a href='https://github.com/' target='_Blank'> Aqui</a> </label></ListGroup.Item>
+                        <ListGroup.Item className='font-bold'> <label>Codigo Fuente Front: <a href={FrontGitHub} target='_Blank'> Aqui</a> </label></ListGroup.Item>
+                        <ListGroup.Item className='font-bold'> <label>Codigo Fuente Api: <a href={ApiGitHub} target='_Blank'> Aqui</a> </label></ListGroup.Item>
 
-                        <ListGroup.Item className='font-bold'> <label>Api: <a href='https://github.com/' target='_Blank'> Aqui</a> </label></ListGroup.Item>
+                        <ListGroup.Item className='font-bold'> <label>Valorar prueba tecnica <a href={UrlEncuesta} target='_Blank'> Aqui</a> </label></ListGroup.Item>
 
-                        <ListGroup.Item className='font-bold'> <label>Valorar prueba tecnica <a href='https://github.com/' target='_Blank'> Aqui</a> </label></ListGroup.Item>
-
-                        <ListGroup.Item className='font-bold'> <label>Telemedik: <a href='https://github.com/' target='_Blank'> Aqui</a> </label></ListGroup.Item>
+                        <ListGroup.Item className='font-bold'> <label>Telemedik: <a href={TelemedikLink} target='_Blank'> Aqui</a> </label></ListGroup.Item>
                     </ListGroup>
                 </Modal.Body>
                 <Modal.Footer>
@@ -70,9 +68,9 @@ function Menu(props) {
                         <ListGroup.Item className='text-center font-bold'>Inicia sesion</ListGroup.Item>
                         <ListGroup.Item className='font-bold'> <label>Codigo Fuente Front: <a href={FrontGitHub} target='_Blank'> Aqui</a> </label></ListGroup.Item>
                         <ListGroup.Item className='font-bold'> <label>Codigo Fuente Api: <a href={ApiGitHub} target='_Blank'> Aqui</a> </label></ListGroup.Item>
-                        <ListGroup.Item className='font-bold'> <label>Valorar prueba tecnica <a href={UrlEncuesta}target='_Blank'> Aqui</a> </label></ListGroup.Item>
+                        <ListGroup.Item className='font-bold'> <label>Valorar prueba tecnica <a href={UrlEncuesta} target='_Blank'> Aqui</a> </label></ListGroup.Item>
 
-                        <ListGroup.Item className='font-bold'> <label>Telemedik: <a href='https://github.com/' target='_Blank'> Aqui</a> </label></ListGroup.Item>
+                        <ListGroup.Item className='font-bold'> <label>Telemedik: <a  href={TelemedikLink}  target='_Blank'> Aqui</a> </label></ListGroup.Item>
                     </ListGroup>
                 </Modal.Body>
                 <Modal.Footer>
